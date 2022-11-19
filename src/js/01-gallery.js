54bf06eba27ba8d13e82e73d86ac1d5c99d9a069
Yana Bertnik <yana.bertnik@ukr.net>
2022-11-19 09:06:13 +0200

    add 8 hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const imageMarkup = createImageList(galleryItems);
+gallery.insertAdjacentHTML('beforeend', imageMarkup);
+gallery.addEventListener('click', onClickImg);
+
+function createImageList(e) {
+    return e.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `
+    })
+        .join('');
+}
+
+function onClickImg(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
