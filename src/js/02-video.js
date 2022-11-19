a3f38029fec4d78658212de65ac8ba5ba26c4e40
Yana Bertnik <yana.bertnik@ukr.net>
2022-11-19 20:26:50 +0200

    add hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,15 @@
-
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay({ seconds }) {
+function onPlay( {seconds} ) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
 
+if (localStorage.getItem('videoplayer-current-time') !== null) {
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,20 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 dataFromLocalStorage();
 
-const formData = {};
+let formData = {};
 
 function onFormData(e) {
-  formData[e.target.name] = e.target.value;
+  formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  formData.email = email.value;
+  formData.message = message.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}
+  }
 
 function onSubmitForm(e) {
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
@@ -21,8 +25,6 @@ function onSubmitForm(e) {
 
 function dataFromLocalStorage() {
   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
   if (data) {
     email.value = data.email;
     message.value = data.message;
